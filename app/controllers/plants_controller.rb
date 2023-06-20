51e93e043c4b7100bb2d06821167657e745cc508
felixNyalenda <felixasejunior@gmail.com>
2023-06-20 18:18:46 +0300

    Completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,47 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
     plants = Plant.all
-    render json: plants
+    render json: plants, except: [:created_at, :updated_at]
   end
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    plant = find_plant
+    render json: plant, except: [:created_at, :updated_at], status: :ok
   end
 
   # POST /plants
   def create
     plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    render json: plant, except: [:created_at, :updated_at], status: :created
+  end
+
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
 
+  def find_plant
+    Plant.find(params[:id])
+  end
+
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_not_found_response
+    render json: { error: "plant not found" }, status: :not_found
   end
 end
